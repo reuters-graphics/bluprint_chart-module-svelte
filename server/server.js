5796beceda12abac97d1093eaf93f4ee3a356106
Jon McClure <jon.r.mcclure@gmail.com>
2021-01-13 11:31:47 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import * as sapper from '@sapper/server';
 
 import compression from 'compression';
 import polka from 'polka';
-import sirv from 'sirv';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
@@ -11,7 +10,6 @@ if (dev) {
   polka()
     .use(
       compression({ threshold: 0 }),
-      sirv('server/static', { dev }),
       sapper.middleware()
     )
     .listen(PORT, err => {
@@ -22,7 +20,6 @@ if (dev) {
     .use(
       '/chart-module-my-chart-module',
       compression({ threshold: 0 }),
-      sirv('server/static', { dev }),
       sapper.middleware()
     )
     .listen(PORT, err => {
